2830914c1a799552a7b28e35598ddfec594673c3
Samuel <sasdallas765@gmail.com>
2025-04-04 17:08:51 -0500

    Doxygen-style comments on GameObject header

--- src/gameobject.hpp
+++ src/gameobject.hpp
@@ -6,8 +6,11 @@
 #define USING_CAMERA true
 
 
-/// @brief Namespace containing primary gameobject related core functionality
+/**
+ * @brief Contains primary GameObject functionality
+ */
 namespace GameObjects {
+    // Limits
     #define GAME_OBJECT_CHILD_LIMIT 16
     #define GAME_OBJECT_COMPONENT_LIMIT 8
     #define GAME_OBJECT_MANAGED_REFERNECE_LIMIT 4
@@ -18,66 +21,138 @@ namespace GameObjects {
         return (*str) ? typeIdHasher(str + 1, (hash ^ static_cast<uint32_t>(*str)) * 16777619u) : hash;
     }
 
+    // Prototypes
     struct Component;
     struct GameObject;
 
-    /// @brief Handles game object management
+    /**
+     * @brief Handles game object management
+     */
     struct GameManager {
         std::set<GameObject*> activeObjects = {};
         std::set<GameObject*> freeQueue = {};
         controller_data controllerState;
-        // std::map<GameObject*, GameObject*[4]> references = {};
         surface_t* display;
         #if USING_CAMERA
         Vector2f camera_pos;
         #endif
 
+        /**
+         * @brief Requests the destruction of a game object next frame
+         * @param gameObject The object to destroy
+         */
         void requestFree(GameObject* gameObject);
+
+        /**
+         * @brief Update method
+         */
         void update();
+        
         void setRoot();
-        // void addManagedReference(GameObject* refering, GameObject* reference);
     };
 
+    /**
+     * @brief Component of a game object
+     */
     struct Component {
+        // Frames active
         int framesActive = 0;
+
+        // Game object parent
         GameObject* gameObject;
+        
+        // Component active 
+        bool active;
+
+        /**
+         * @brief Update method for a component
+         * @param dt Delta time
+         */
         virtual void update(float dt) {
             framesActive++;
             if(framesActive == 1) {
                 ready();
             }
         }
+
+        /**
+         * @brief Render method
+         */
         virtual void render() {
 
         }
+
+        /**
+         * @brief Ready method
+         */
         virtual void ready() {
 
         }
+
+        /**
+         * @brief Called on child freed
+         */
         virtual void onChildFreed(GameObject* freed_child) {
             
         }
-        bool active;
+
         virtual ~Component() {
 
         }
+
         virtual uint32_t getType() = 0;
     };
 
     struct GameObject {
+        // Frames active
         int framesActive;
+        
+        // List of child components
         Component* components[GAME_OBJECT_COMPONENT_LIMIT];
+
+        // List of child game objects
         GameObject* children[GAME_OBJECT_CHILD_LIMIT];
+        
+        // Parnet gameobject (can be NULL)
         GameObject* parent;
+
+        // List of managed references
         GameObject* managedReferences[GAME_OBJECT_MANAGED_REFERNECE_LIMIT];
+        
+        // Parent game manager
         GameManager* gameManager;
+        
+        // List of components
         int componentCount = 0;
+
+        // Active
         bool active;
 
+        /**
+         * @brief Update method
+         * @param dt Delta time
+         */
         void update(float dt);
+
+        /**
+         * @brief Render method
+         */
         void render();
+        
+        /**
+         * @brief Ready method
+         */
         void ready();
+
+        /**
+         * @brief Called on queue free
+         */
         void queueFree();
 
+        /**
+         * @brief Get component by name
+         * @param name The name of the component to search for
+         */
         template <typename T>
         T* getComponent(const char* name) {
             for (int i = 0; i < GAME_OBJECT_COMPONENT_LIMIT; i++)
@@ -93,9 +168,25 @@ namespace GameObjects {
             }
             return nullptr;
         }
+
+        /**
+         * @brief Add a component
+         * @returns true on success, false otherwise
+         */
         bool addComponent(Component* component);
+
+        /**
+         * @brief Add a child game object
+         * @returns true on success, false otherwise
+         */
         bool addChild(GameObject* child);
+
+        /**
+         * @brief Called on child free
+         * @param freed_child The child which was freed
+         */
         void onChildFreed(GameObject* freed_child);
+        
         ~GameObject();
         GameObject();
     };
@@ -103,7 +194,7 @@ namespace GameObjects {
     #define COMPONENT(type) struct type : public GameObjects::Component { \
         uint32_t getType() override { \
             return GameObjects::typeIdHasher(#type); \
-        }
+        } 
     
     #define GET_COMPONENT(type) getComponent<type>(#type)
 };
